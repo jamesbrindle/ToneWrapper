2783f3c3fdb923ccc5ae2e6cff0d36b61da17ff9
Jamie Brindle <jamie.brindle7@gmail.com>
2024-06-22 18:22:24 +0100

    Add project files.

---
+++ ToneWrapper/Helpers/ProcessHelper.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace ToneWrapperApp.Helpers
+{
+    public class ProcessHelper
+    {
+        /// <summary>
+        /// Executes a process and reads the output (std out and error out) - Useful for working with command line utilties in managed code
+        /// </summary>
+        /// <param name="path">Path of program to execute</param>
+        /// <param name="arguments">Optionally include and command line arguments</param>
+        /// <param name="workingDirectory">Working folder path</param>
+        /// <param name="timeoutSeconds">End process timeout. Defaults to 30 seconds. Set to -1 for no timeout (infinite)</param>
+        /// <param name="throwOnError">Should the method throw an error when exception caught or just ignore</param>
+        /// <returns>Return STD + Err output</returns>
+        public static string ExecuteProcessAndReadStdOut(
+            string path,
+            out string errorOutput,
+            string arguments = "",
+            string workingDirectory = "",
+            int timeoutSeconds = 60,
+            bool throwOnError = true)
+        {
+            var _outputStringBuilder = new StringBuilder();
+            var _errorStringBuilder = new StringBuilder();
+            int timeoutInMs = timeoutSeconds * 1000;
+
+            var process = new Process();
+
+            try
+            {
+                process.StartInfo.FileName = path;
+                process.StartInfo.Arguments = arguments;
+                process.StartInfo.WorkingDirectory = string.IsNullOrEmpty(workingDirectory) ? Path.GetDirectoryName(path) : workingDirectory;
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.UseShellExecute = false;
+
+                using (var outputWaitHandle = new AutoResetEvent(false))
+                using (var errorWaitHandle = new AutoResetEvent(false))
+                {
+                    void outputHandler(object sender, DataReceivedEventArgs e)
+                    {
+                        if (e.Data == null)
+                            outputWaitHandle.Set();
+                        else
+                            _outputStringBuilder.Append(e.Data);
+                    }
+
+                    void errorHandler(object sender, DataReceivedEventArgs e)
+                    {
+                        if (e.Data == null)
+                            errorWaitHandle.Set();
+                        else
+                        {
+                            if (!string.IsNullOrWhiteSpace(e.Data) && !e.Data.Contains("diacritics") &&
+                                !string.IsNullOrWhiteSpace(e.Data) && !e.Data.Contains("symbol") &&
+                                !string.IsNullOrWhiteSpace(e.Data) && !e.Data.Contains("ZapfDingbats"))
+                                _errorStringBuilder.Append(e.Data);
+                        }
+                    }
+
+                    process.OutputDataReceived += outputHandler;
+                    process.ErrorDataReceived += errorHandler;
+
+                    try
+                    {
+                        process.Start();
+                        process.BeginOutputReadLine();
+                        process.BeginErrorReadLine();
+
+                        if (timeoutSeconds == -1)
+                        {
+                            process.WaitForExit();
+                            _errorStringBuilder.Append("Exit: " + process.ExitCode);
+                        }
+                        else
+                        {
+                            if (process.WaitForExit(timeoutInMs) &&
+                                outputWaitHandle.WaitOne(timeoutInMs) &&
+                                errorWaitHandle.WaitOne(timeoutInMs))
+                            {
+                                if (process.ExitCode != 0)
+                                    _errorStringBuilder.Append("Exit: " + process.ExitCode);
+                            }
+                            else
+                            {
+                                if (!process.HasExited)
+                                {
+                                    process.Kill();
+                                    throw new Exception("ERROR: Process took too long to finish");
+                                }
+                            }
+                        }
+                    }
+                    finally
+                    {
+                        process.OutputDataReceived -= outputHandler;
+                        process.ErrorDataReceived -= errorHandler;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                if (throwOnError)
+                    throw new Exception("Execution error: " + e.Message);
+            }
+            finally
+            {
+                process.Close();
+                try
+                {
+                    process.Kill();
+                }
+                catch { }
+            }
+
+            errorOutput = _errorStringBuilder.ToString();
+            return _outputStringBuilder.ToString();
+        }
+    }
+}
---
+++ ToneWrapper/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToneWrapperApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var tone = new ToneWrapper(@"E:\Downloads\Recursion-LC_128_44100_stereo.m4b");
+
+            tone.Artist = "Blake Crouch";
+            tone.Title = "Recursion";
+            tone.OriginalAlbum = "Recursion";
+            tone.AlbumArtist = "Blake Crouch";            
+            tone.Genre = "Audiobook";
+            tone.Comment = "Memory makes reality. That's what NYC cop Barry Sutton is learning, as he investigates the devastating phenomenon the media has dubbed False Memory Syndrome—a mysterious affliction that drives its victims mad with memories of a life they never lived. That's what neuroscientist Helena Smith believes. It's why she's dedicated her life to creating a technology that will let us preserve our most precious memories. If she succeeds, anyone will be able to re-experience a first kiss, the birth of a child, the final moment with a dying parent. As Barry searches for the truth, he comes face to face with an opponent more terrifying than any disease—a force that attacks not just our minds, but the very fabric of the past. And as its effects begin to unmake the world as we know it, only he and Helena, working together, will stand a chance at defeating it. But how can they make a stand when reality itself is shifting and crumbling all around them? At once a relentless pageturner and an intricate science-fiction puzzlebox about time, identity, and memory, Recursion is a thriller as only Blake Crouch could imagine it—and his most ambitious, mind-boggling, irresistible work to date.";
+            tone.Description = "Memory makes reality. That's what NYC cop Barry Sutton is learning, as he investigates the devastating phenomenon the media has dubbed False Memory Syndrome—a mysterious affliction that drives its victims mad with memories of a life they never lived. That's what neuroscientist Helena Smith believes. It's why she's dedicated her life to creating a technology that will let us preserve our most precious memories. If she succeeds, anyone will be able to re-experience a first kiss, the birth of a child, the final moment with a dying parent. As Barry searches for the truth, he comes face to face with an opponent more terrifying than any disease—a force that attacks not just our minds, but the very fabric of the past. And as its effects begin to unmake the world as we know it, only he and Helena, working together, will stand a chance at defeating it. But how can they make a stand when reality itself is shifting and crumbling all around them? At once a relentless pageturner and an intricate science-fiction puzzlebox about time, identity, and memory, Recursion is a thriller as only Blake Crouch could imagine it—and his most ambitious, mind-boggling, irresistible work to date.";
+            tone.Composer = "Jon Lindstrom;  Abby Craden";
+            tone.Narrator = "Jon Lindstrom;  Abby Craden";
+            tone.Copyright = "©2019 Blake Crouch (P)2019 Penguin Random House LLC";
+            tone.TrackNumber = "1";
+            tone.TrackTotal = "1";
+            tone.SortAlbum = "Recursion";
+            tone.RecordingDate = "01/01/2019";
+            tone.AdditionalFieldsToAdd.Add(new ToneWrapper.AdditionalField("ASIN", "1509866701"));
+            tone.AdditionalFieldsToAdd.Add(new ToneWrapper.AdditionalField("YEAR", "2019"));
+
+            tone.WriteMetaData();
+        }
+    }
+}
